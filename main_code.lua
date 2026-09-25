353015d30c09bd5b7148d95483a288a4855f3643
smab44 <317313487+smab44@users.noreply.github.com>
2026-09-25 17:35:26 +0300

    הוספתי אנמציות והורדתי את השעון למוד של השחקן מכיוון שהאנמציות מתפלות בזה עכשיו

--- main_code.lua
+++ main_code.lua
@@ -1,36 +1,58 @@
 
 function _init()
-	make_player()
-	player_state = "idle"
+    --player pos variables--
+    p={
+    x=50,
+    y=50,
+    }
+
+    --attack timer and state--
+    player_state = "idle"
     attack_timer = 0
+
+    --animation--
+    valid_frames={0,2,4,6,8,10,12}
+    frame = 1
+    speed = .3
+
 end
 
-function _update60()
-	player_attack()
 
-    if attack_timer > 0 then
-        attack_timer -= 1
-    elseif attack_timer == 0 then
-        player_state = "idle"
-    else
-        player_state = "idle"
+
+function _update60()
+    player_attack()
+
+    --attack timer an state--
+--    if attack_timer > 0 then
+--        attack_timer -= 1
+--    else
+--        player_state = "idle"
+--    end
+
+    --animation--
+    if player_state == "attacking" then
+        if frame < 12-speed then --chosing a frame number from valid_frames{}--
+            frame +=speed
+        else    
+            frame = 1
+            player_state = "idle"
+        end
+    elseif player_state == "idle" then
+        frame = 1
     end
+
+
 
 end
 
 function _draw()
 	cls()
-	spr(1, p.x, p.y, 2, 2)
-	print(player_state)
+	spr(valid_frames[flr(frame)], p.x, p.y, 2, 2)
+	--print(player_state)
+    print(frame)
 end
 
 
-
-function make_player()
-	p={}
-	p.x =50
-	p.y =50
-end
 
 function player_attack()
 	if btn(4) then
